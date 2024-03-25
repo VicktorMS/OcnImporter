9921b49f0325877843465458dfc18ad4bd3d1425
victormoraes <victor.moraesrj@gmail.com>
2024-03-25 12:20:56 -0300

    On click import button converts import string array to with convert_string_array_to_import_data_list()

--- src/ui/controllers/button_handler.py
+++ src/ui/controllers/button_handler.py
@@ -1,4 +1,4 @@
-from src.ui.controllers.main_controller import MainController
+from src.core.import_list_converter import convert_string_array_to_import_data_list
 
 
 class ButtonHandler:
@@ -6,38 +6,29 @@ class ButtonHandler:
         self.main_window = MainWindow
         self.main_controller = MainController
 
-        self.on_paste_button_clicked()
-        self.on_copy_button_clicked()
-        self.on_import_button_clicked()
+        self.setup_button_callbacks()
+        self.hide_development_buttons()
 
-        # Development
-        self.hide_in_development_buttons()
+    def setup_button_callbacks(self):
+        self.setup_paste_button_callback()
+        self.setup_import_button_callback()
+        self.setup_copy_button_callback()
 
-    def disable_button_if_invalid_inputs(self):
-        pass
-
-    def on_paste_button_clicked(self):
+    def setup_paste_button_callback(self):
         self.main_window.paste_import_list_btn.clicked.connect(lambda: print("Paste Btn"))
 
-    def on_import_button_clicked(self):
-        self.main_window.import_btn.clicked.connect(
-            lambda: print(
-                "Source path: ", self.main_controller.source_dir_path,
-                "\nDestination path: ", self.main_controller.destination_dir_path,
-                "\nImport List: ", self.main_controller.import_list
-            ))
+    def setup_import_button_callback(self):
+        self.main_window.import_btn.clicked.connect(lambda: self.process_data_string())
 
-    def on_copy_button_clicked(self):
+    def process_data_string(self):
+        string_array = self.main_controller.import_list
+        import_list = convert_string_array_to_import_data_list(string_array)
+        print("Hello ", import_list)
+
+    def setup_copy_button_callback(self):
         self.main_window.copy_import_results_btn.clicked.connect(lambda: print("Copy Btn"))
 
-
-
-
-
-
-
-    # This method is only for development
-    def hide_in_development_buttons(self):
+    def hide_development_buttons(self):
         self.main_window.check_import_btn.hide()
         self.main_window.use_import_list_file_btn.hide()
         self.main_window.export_import_results_btn.hide()
